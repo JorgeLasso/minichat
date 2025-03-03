ce28387b544a491259f563d0e051ba26e11c7741
Jorge Mario Lasso Ortiz <jorgelasso_1992@hotmail.com>
2025-03-03 09:51:34 -0500

    Configured CORS

--- models/server.js
+++ models/server.js
@@ -3,6 +3,7 @@ const http = require("http");
 const socketio = require("socket.io");
 const path = require("path");
 const Sockets = require("./sockets");
+const cors = require("cors");
 
 class Server {
   constructor() {
@@ -22,6 +23,9 @@ class Server {
   middlewares() {
     //Deploy Public Directory
     this.app.use(express.static(path.resolve(__dirname, "../public")));
+
+    //CORS
+    this.app.use(cors());
   }
 
   //Socket Configuration
